a73c80afa4076dec3354d1dd09d671892b07af71
Avery Briggs <abriggs1@unb.ca>
2018-10-16 09:55:28 -0300

    2018-10-16 working on summarize_mail.js

--- Assignments/Assignment3/summarize_mail.js
+++ Assignments/Assignment3/summarize_mail.js
@@ -7,20 +7,20 @@ let lst = null;
 function summarize_mail(filename){
     data = null;
     lst = null;
-    if (data===null){}
+    if (data===null){
     //data=read_json_file("ancestry.json"); //for ancestry.spec.js tests
-        data=flatten(read_json_file(filename)); //for shuffle tests
-        if(arguments.length <= 1){
+        data = flatten(read_json_file(filename)); //for shuffle tests
+        if(arguments.length < 2){
           console.log("No arguments given.");
-          return data;
+          return flatten(data);
         }
       // simple linear scan
-      console.log("\tflatten(data): " + data + ",\n\targuments.length: " + arguments.length);
+      console.log("\tdata: " + data + ",\n\targuments.length: " + arguments.length);
       for(var k = 0; k < arguments.length; k++){
-        console.log("k: " + k + " arguments.length: " + arguments.length + ", arguments[k]: " + arguments[k]);
+        //console.log("k: " + k + " arguments.length: " + arguments.length + ", arguments[k]: " + arguments[k]);
       }
       for(var k = 0; k < data.length; k++){
-        console.log("k: " + k + " data.length: " + data.length + ", data[k].id: " + data[k].arguments[1]);
+        c//onsole.log("k: " + k + " data.length: " + data.length + ", data[k].id: " + data[k].arguments[1]);
       }
       for(let i = 0; i < data.length; i++){
           for(let j = 1; j < arguments.length; j++){
@@ -31,6 +31,7 @@ function summarize_mail(filename){
           }
       }
     }
+    console.log("RETURNING LIST");
     return lst;
     //read_json_file("example1.json");
 }
