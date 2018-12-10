c99de414ca0395452f1bebdcefed47a8a21f11be
Avery <abriggs1@unb.ca>
2018-12-10 16:04:42 -0400

    2018-12-10 completed sample questions

--- ExamPrep/isolated.m
+++ ExamPrep/isolated.m
@@ -1,23 +1,14 @@
 function out = isolated(img)
-  img
-  x = nbrcount(img)
-  a = size(x,1)
-  b = size(x,2)
-  for i = 1 : a
-    for j = 1 : b
-      i
-      j
-      if x(i,j) > 1
-        x(i,j) = 0
-      endif
-      if x(i,j) == 1
-        x(i,j) = 0
-      elseif x(i,j) == 0
-        x(i,j) = 1
+  x = nbrcount(img);
+  for i = 1 : rows(x)
+    for j = 1 : columns(x)
+      if x(i,j) > 0
+        x(i,j) = 0;
+      elseif img(i,j) == 1
+        x(i,j) = 1;
       endif
     endfor
   endfor
-  %arrayfun(nbrcount, img)
   out = x
 endfunction
 
